fbb2ad2f0798c3f9ad376c6a9c4bc6ced91d7d00
anpudova <90646684+anpudova@users.noreply.github.com>
2022-07-02 12:42:59 +0300

    Task done

--- MyTask1/app/src/main/java/com/example/mytask_1/MainActivity.kt
+++ MyTask1/app/src/main/java/com/example/mytask_1/MainActivity.kt
@@ -11,7 +11,6 @@ import java.lang.Double
 
 class MainActivity : Activity() {
 
-    @SuppressLint("UseValueOf")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
